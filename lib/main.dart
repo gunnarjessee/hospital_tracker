a854aebb4b52f194884eebb59c6f5d5c89394281
Gunnar Jessee <gunnarjessee@gmail.com>
2022-02-25 21:39:27 -0500

    added hospital widget
    Added hospital widget to allow adding more hospitals through an array. hospitals can increment and decrement amount of times visited. needs to add to local storage.

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,9 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+  final hospitals = ['Pulaski', 'Montgomery', 'Alleghany', 'Salem', 'Cave', 'RMH', 'NRV', 'Out of Town'];
+
   @override
   State<StatefulWidget> createState() => _HomePage();
 
@@ -38,10 +41,68 @@ class _HomePage extends State<HomePage> {
         backgroundColor: Colors.deepOrange,),
       body:  MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Text('Hospitals', style: TextStyle(decoration: TextDecoration.none, color: Colors.deepOrange),),
+        home: HospitalCounter('Pulaski', 10),
       ),
     );
   }
 
+}
+
+class HospitalCounter extends StatefulWidget {
+  String name = "";
+  int number = 0;
+
+  HospitalCounter(this.name, this.number, {Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HospitalCounter();
+  
+}
+
+class _HospitalCounter extends State<HospitalCounter> {
+
+  final TextStyle _style = const TextStyle(decoration: TextDecoration.none, color: Colors.greenAccent, fontSize: 20);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GridView.count(
+          crossAxisCount: 4,
+          children: [
+            Container(
+              width: 1000,
+              child: Center(child: Text("" + widget.name.toString(), style: _style,),),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () { setState(() {
+                  widget.number++;
+                }); },
+                child: const Text("+", style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.greenAccent,
+                    fontSize: 24
+                ),),),
+            ),
+            Container(
+              child: TextButton(
+                onPressed: () { setState(() {
+                  widget.number = widget.number - 1;
+                }); },
+                child: const Text("-", style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: Colors.greenAccent,
+                    fontSize: 24
+                ),),
+              ),
+            ),
+            Container(
+              child: Center(child: Text('' + widget.number.toString(), style: _style,),),
+            )
+          ],
+      ),
+    );
+  }
+  
 }
 
